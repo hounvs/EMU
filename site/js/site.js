8404e69aef0f19a18071c085c7f8eed6b411fbfa
hounvs <hounvs@gmail.com>
2016-01-14 16:48:20 -0500

    Set defaults

--- site/js/site.js
+++ site/js/site.js
@@ -16,22 +16,31 @@ var urlParams;
     window.onpopstate();
 })();
 
-if(urlParams.streamName) {   //if streamName has truthy value
-    connectStr = 'http://164.76.124.33:1935/live/' + urlParams.streamName + '/playlist.m3u8';
-} else if(urlParams.fileName) { //if fileName has truthy value
-    connectStr = 'http://164.76.124.33:1935/vod/mp4:' + urlParams.fileName + '/playlist.m3u8';
-}
-
 function loadPlayer() {
+    // Defaults
+    var connectionString = '';
+    var posterImage = 'images/emu-logo.jpg';
+    var playerColor = "#006234";
+    var playerHeight = 480;
+    var playerWidth = 720;
+    var autoPlay = false;
+    
+    if(urlParams.streamName) {   //if streamName has truthy value
+        connectionString = 'http://164.76.124.33:1935/live/' + urlParams.streamName + '/playlist.m3u8';
+        autoPlay = true;
+    } else if(urlParams.fileName) { //if fileName has truthy value
+        connectionString = 'http://164.76.124.33:1935/vod/mp4:' + urlParams.fileName + '/playlist.m3u8';
+    }
+    
     var playerElement = document.getElementById("player-wrapper");
 
     var player = new Clappr.Player({
-        source: connectStr,
-        poster: 'images/emu-logo.jpg',
-        height: 480,
-        width: 720,
-        mediacontrol: {seekbar: "#006234", buttons: "#006234"},
-        autoPlay: false
+        source: connectionString,
+        poster: posterImage,
+        height: playerHeight,
+        width: playerWidth,
+        mediacontrol: {seekbar: playerColor, buttons: playerColor},
+        autoPlay: autoPlay
     });
 
     player.attachTo(playerElement);
