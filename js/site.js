85573341d935a44e61e7b7b3cfbef666e062c4ca
hounvs <hounvs@gmail.com>
2016-03-21 09:47:04 -0400

    Add EMU-CMTA logo, allow poster changing, and add VOD support

--- js/site.js
+++ js/site.js
@@ -1,43 +1,53 @@
 // Grabs the query strings and puts them into urlParams
 var urlParams;
 
-(function() {
+(function () {
     window.onpopstate = function () {
         var match,
-        pl     = /\+/g,  // Regex for replacing addition symbol with a space
-        search = /([^&=]+)=?([^&]*)/g,
-        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = window.location.search.substring(1);
+            pl = /\+/g,  // Regex for replacing addition symbol with a space
+            search = /([^&=]+)=?([^&]*)/g,
+            decode = function (s) {
+                return decodeURIComponent(s.replace(pl, " "));
+            },
+            query = window.location.search.substring(1);
 
         urlParams = {};
-        while (match = search.exec(query))
-        urlParams[decode(match[1])] = decode(match[2]);
-    }
+        while (match = search.exec(query)) {
+            urlParams[decode(match[1])] = decode(match[2]);
+        }
+    };
     window.onpopstate();
-})();
+}());
 
-function loadPlayer() {
+function loadPlayer(newPosterImage) {
     // Defaults
     var connectionString = '';
-    var posterImage = 'images/emu-logo.jpg';
+    var posterImage = newPosterImage;
     var playerColor = "#006234";
     var playerHeight = 480;
     var playerWidth = 720;
     var autoPlay = false;
-    
+
     // Check if stream name or file path/name are defined
-    if(urlParams.stream) {   // if stream has truthy value
+    if (urlParams.stream) {   // if stream has truthy value
+        // http://164.76.124.68/stream/index.html?stream=test
         connectionString = 'http://164.76.124.33:1935/live/' + urlParams.stream + '/playlist.m3u8';
         autoPlay = true;
-    } else if(urlParams.path && urlParams.file) { // if path and file have truthy values
+    } else if (urlParams.path && urlParams.file) { // if path and file have truthy values
+        // http://164.76.124.68/stream/index.html?path=vod&file=sample
         connectionString = 'http://164.76.124.33:1935/' + urlParams.path + '/mp4:' + urlParams.file + '/playlist.m3u8';
     }
     
+    // If posterImage didn't get a valid value, assign default
+    if(!posterImage) {
+        posterImage = 'images/emu-logo.jpg';
+    }
+
     // Get player wrapper
     var playerElement = document.getElementById("player-wrapper");
-    
+
     // if connectionStringhas truthy value
-    if(connectionString) {
+    if (connectionString) {
         // Build the player
         var player = new Clappr.Player({
             source: connectionString,
@@ -47,7 +57,7 @@ function loadPlayer() {
             mediacontrol: {seekbar: playerColor, buttons: playerColor},
             autoPlay: autoPlay
         });
-        
+
         // Populate player container
         player.attachTo(playerElement);
     } else {    // show the menu
